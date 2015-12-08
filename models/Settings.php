54e3ee19f1fdca8c762ca24f34346db17abb1ced
GreenImp <lee@greenimp.co.uk>
2015-12-08 11:56:42 +0000

    Adds settings model file and field declarations

---
+++ models/Settings.php
@@ -0,0 +1,21 @@
+<?php namespace GreenImp\Offices\Models;
+
+use Model;
+use Cms\Classes\Page;
+
+/**
+ * Settings Model
+ * @link https://octobercms.com/docs/database/model
+ */
+class Settings extends Model
+{
+  public $implement       = ['System.Behaviors.SettingsModel'];
+
+  public $settingsCode    = 'greenimp_offices_code';
+
+  public $settingsFields  = 'fields.yaml';
+
+  public function getGroupPageOptions(){
+    return Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+  }
+}
